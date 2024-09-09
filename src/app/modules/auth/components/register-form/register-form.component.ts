06a960720430762b5d4b02fba177a58d22c1c43f
Bryan Lopez <bryansyng2@gmail.com>
2024-09-09 16:56:27 -0600

    Agregando validación de usuario disponible

--- src/app/modules/auth/components/register-form/register-form.component.ts
+++ src/app/modules/auth/components/register-form/register-form.component.ts
@@ -12,24 +12,30 @@ import { CustomValidators } from '@utils/validators';
   templateUrl: './register-form.component.html',
 })
 export class RegisterFormComponent {
+
+  formUser = this.formBuilder.nonNullable.group({
+    email: ['', [Validators.email, Validators.required]]
+  });
   form = this.formBuilder.nonNullable.group({
     name: ['', [Validators.required]],
     email: ['', [Validators.email, Validators.required]],
     password: ['', [Validators.minLength(8), Validators.required]],
     confirmPassword: ['', [Validators.required]],
   }, {
-    validators: [ CustomValidators.MatchValidator('password', 'confirmPassword') ]
+    validators: [CustomValidators.MatchValidator('password', 'confirmPassword')]
   });
   status: RequestStatus = 'init';
+  statusUser: RequestStatus = 'init';
   faEye = faEye;
   faEyeSlash = faEyeSlash;
   showPassword = false;
+  showRegister = false;
 
   constructor(
     private formBuilder: FormBuilder,
     private router: Router,
     private authService: AuthService
-  ) {}
+  ) { }
 
   register() {
     if (this.form.valid) {
@@ -47,6 +53,32 @@ export class RegisterFormComponent {
         });
     } else {
       this.form.markAllAsTouched();
+    }
+  }
+
+  validateUser() {
+    if (this.formUser.valid) {
+      this.statusUser = 'loading';
+      const { email } = this.formUser.getRawValue();
+      this.authService.isAvailable(email)
+        .subscribe({
+          next: (response) => {
+            if (response.isAvailable) {
+              this.statusUser = 'success';
+              this.form.controls.email.setValue(email);
+              this.showRegister = true;
+            } else {
+              this.router.navigate(['/login'], {
+                queryParams: { email }
+              });
+            }
+          },
+          error: () => {
+            this.statusUser = 'failed';
+          }
+        });
+    } else {
+      this.formUser.markAllAsTouched();
     }
   }
 }
