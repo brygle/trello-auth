437ed8d9f8f70dbe26432ab276aa882957c9709b
Bryan Lopez <bryansyng2@gmail.com>
2024-09-10 15:46:43 -0600

    Agregando listado de usuarios

--- src/app/modules/users/pages/users-table/data-source.ts
+++ src/app/modules/users/pages/users-table/data-source.ts
@@ -1,9 +1,10 @@
 import { DataSource } from '@angular/cdk/collections';
+import { User } from '@models/user.model';
 import { BehaviorSubject, Observable } from 'rxjs';
 
 export class DataSourceUser extends DataSource<any[]> {
 
-  data = new BehaviorSubject<any[]>([]);
+  data = new BehaviorSubject<User[]>([]);
   originalData: any[]= [];
 
   connect(): Observable<any[]> {
--- src/app/modules/users/pages/users-table/users-table.component.ts
+++ src/app/modules/users/pages/users-table/users-table.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 import { DataSourceUser } from './data-source';
+import { UsersService } from '@services/users.service';
 
 @Component({
   selector: 'app-users-table',
@@ -11,27 +12,18 @@ export class UsersTableComponent  {
   dataSource = new DataSourceUser();
   columns: string[] = ['id', 'avatar', 'name', 'email'];
 
-  constructor() {
-    this.dataSource.init([
-      {
-        id: 1,
-        name: 'User 1',
-        email: 'mail@mail.com',
-        avatar: 'https://api.lorem.space/image/face?w=150&h=150'
-      },
-      {
-        id: 2,
-        name: 'User 2',
-        email: 'mail2@mail.com',
-        avatar: 'https://api.lorem.space/image/face?w=150&h=150'
-      },
-      {
-        id: 3,
-        name: 'User 3',
-        email: 'mail3@mail.com',
-        avatar: 'https://api.lorem.space/image/face?w=150&h=150'
-      }
-    ]);
+  constructor(
+    private usersService: UsersService
+  ) { }
+
+  ngOnInit(): void {
+    this.usersService.getUsers()
+      .subscribe({
+        next: (users) => {
+          this.dataSource.init(users);
+        },
+        error: () => {}
+      });
   }
 
 }
