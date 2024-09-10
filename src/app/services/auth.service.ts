aee9a1288112f05645def46b52b1f62e33d04429
Bryan Lopez <bryansyng2@gmail.com>
2024-09-10 17:23:22 -0600

    Agregando refresh token

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -26,7 +26,8 @@ export class AuthService {
       password
     }).pipe(
       tap((response) => {
-        this.tokenService.saveToken(response.access_token)
+        this.tokenService.saveToken(response.access_token);
+        this.tokenService.saveRefreshToken(response.refresh_token);
       })
     );
   }
@@ -67,6 +68,16 @@ export class AuthService {
       .pipe(
         tap((user) => {
           this.user$.next(user);
+        })
+      );
+  }
+
+  refreshToken(refreshToken: string) {
+    return this.http.post<ResponseLogin>(`${this.apiUrl}/api/v1/auth/refresh-token`, {refreshToken})
+      .pipe(
+        tap(response => {
+          this.tokenService.saveToken(response.access_token);
+          this.tokenService.saveRefreshToken(response.refresh_token);
         })
       );
   }
